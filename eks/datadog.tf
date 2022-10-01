88107686593c927518f1344a71037965666a3ac5
jdial1996 <jdial1996@yahoo.co.uk>
2022-10-01 13:47:24 +0100

    add datadog

---
+++ eks/datadog.tf
@@ -0,0 +1,22 @@
+
+# This helm chart will install the datadog agent daemonset into your cluster
+# so that Datadog is able to push application events, logs and metrics to datadoghq.
+
+# You will also have to install the Kubernetes datadog integration in the datadog console to be able to 
+# view kubernetes related data in Datadog
+
+resource "helm_release" "datadog" {
+  name       = "datadog"
+  repository = "https://helm.datadoghq.com"
+  chart      = "datadog"
+  version    = "3.1.3"
+
+  values = [
+    "${file("values/datadog-values.yaml")}"
+  ]
+
+  set {
+    name  = "datadog.apiKey"
+    value = ""
+  }
+}
